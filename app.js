607038eb5ae2b1fa1976160775495f63e7cf7110
Kieran Lewis <kieranlewis1334@gmail.com>
2016-03-25 13:31:05 +1300

    can now do an xquery search

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var search = require('./routes/search');
+var results = require('./routes/results');
 
 var app = express();
 
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/search', search);
+app.use('/results', results);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/search.js
+++ routes/search.js
@@ -6,25 +6,15 @@ var client = new basex.Session('127.0.0.1', 1984, 'admin', 'admin');
 
 client.execute("OPEN Colenso");
 
-/* GET search listing. */
+/* GET search page. */
 router.get('/', function(req, res) {
   	res.render('search', { title: 'Search' });
 });
 
-/*
-router.get("/",function(req,res){
-	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-	" (//name[@type='place'])[1] ",
-	function (error, result) {
-		if(error){ 
-			console.error(error);
-		} else {
-			res.render('search', { title: 'Search', result: result.result });
-		}
-	}
-	);
+
+router.get('/results', function(req, res) {
+	res.render('results', { title: 'Results', result: req.query.searchString })
 });
-*/
 
 module.exports = router;
 
